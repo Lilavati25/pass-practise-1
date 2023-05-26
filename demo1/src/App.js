b2f801a85ac0b3c0c7eb4f674830c5be18c6764b
Lilavati25 <85790740+Lilavati25@users.noreply.github.com>
2023-05-26 11:54:14 +0530

    third commit

--- demo1/src/App.js
+++ demo1/src/App.js
@@ -10,13 +10,6 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Lilavati Mhaske</p>
-        <a
-          className="App-link"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
